26a6e4ef32e91a7a56c25442b6b67b3fafc0da9d
Juan Rosario <juanelias18@hotmail.com>
2019-08-30 15:12:15 -0400

    Conexion con la base de datos

---
+++ DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Entidades;
+using System;
+using System.Data.Entity;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuario { get; set; }
+        public DbSet<Clientes> Cliente { get; set; }
+        public DbSet<Inventarios> Inventario { get; set; }
+        public DbSet<Productos> Producto { get; set; }
+        public DbSet<Proveedores> Proveedor { get; set; }
+        public DbSet<Secciones> Seccion { get; set; }
+        public DbSet<Ventas> Venta { get; set; }
+        public DbSet<VentasDetalle> Detalle { get; set; }
+        public Contexto() : base("ConStr") { }
+    }
+}
